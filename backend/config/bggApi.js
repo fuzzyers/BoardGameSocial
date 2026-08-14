34838125174e09a88c86469abac1e11289350b15
Jackson Williamson <jackiemyson@gmail.com>
2026-08-14 14:29:45 +1200

    Added a queue to all bgg calls

---
+++ backend/config/bggApi.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import PQueue from "p-queue";
+
+const bggToken = process.env.BGG_API_TOKEN;
+
+if (!bggToken) {
+    throw new Error("BGG API token is not defined.");
+}
+
+const bggQueue = new PQueue({
+    concurrency: 1,
+    intervalCap: 1,
+    interval: 5000,
+});
+
+export const axiosInstance = axios.create({
+    baseURL: "https://boardgamegeek.com/xmlapi2",
+    headers: {
+        Authorization: `Bearer ${bggToken}`,
+    },
+});
+
+export const bggApi = {
+    get: (url, config) =>
+        bggQueue.add(() => axiosInstance.get(url, config)),
+};
--- backend/services/bgg.js
+++ backend/services/bgg.js
@@ -1,5 +1,5 @@
 import { parseSearchByIdResults, parseSearchResults } from "../utils/xmlparsing.js"
-import { bggApi } from "./bggApi.js"
+import { bggApi } from "../config/bggApi.js"
 
 export const searchBGGGame = async (search) => {
     const response = await bggApi.get("/search", {
@@ -16,6 +16,6 @@ export const searchBGGGameById = async (id) => {
     const response = await bggApi.get(`thing?id=${id}`)
 
     const parsedData = await parseSearchByIdResults(response.data)
-    
+
     return parsedData
 }
